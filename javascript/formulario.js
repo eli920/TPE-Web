d4c352efae6faaacbfc0ac82baab8631e2fbc1a1
Elisa Guaspari <eliguaspari@gmail.com>
2024-05-18 02:44:35 -0300

    Se termina de validar form y se elimina código repetido

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,40 +1,35 @@
 "use strict"
-//Por que no me lo toma de ésta forma??
-// let form=document.querySelector("#form").addEventListener("submit", obtenerDatos);
 
+let random;
+function generarCaptcha(){
+  random= Math.floor(Math.random()*10000);
+  let captchaRandom=document.querySelector("#captcha");
+  captchaRandom.innerHTML= random;
+}
+generarCaptcha();
+
+//Obtenemos la información del formulario y evaluamos que el número random sea igual al numero ingresado por el 
+//usuario en el campo captcha
 let form=document.querySelector("#form");
 form.addEventListener("submit", obtenerDatos);
 
-//Traemos la etiqueta en la cual vamos a ingresar el resultado de un número random
-let captchaRandom=document.querySelector("#captcha");
-let random= Math.floor(Math.random()*1000);
-captchaRandom.innerHTML= random;
-
-//Evaluamos que el nímero random sea igual al numero ingresado por el usuario en el campo captcha
-let captchaIngresado=document.querySelector("#captcha-ingresado").value;
-
-
-if(captchaIngresado===random){
-  document.querySelector("#resultado-captcha").innerHTML= "Correcto"
-}else{
-  document.querySelector("#resultado-captcha").innerHTML= "No existen coincidencias"
-}
-
-
-
 function obtenerDatos(e){
     e.preventDefault();
-    console.log("entre a la funcion");
+    
     let formData = new FormData(form);
     
     let nombre = formData.get('nombre');
     let telefono = formData.get('telefono');
     let email = formData.get('email');
     let consulta= formData.get('consulta');
-    let captchaIngresado= formData.get('numero-captcha')
-
-    
-
-    console.log(nombre, telefono, email, consulta, captchaIngresado);
+    let captchaIngresado= formData.get('captcha-ingresado');
+   
+    if(captchaIngresado==random){
+      document.querySelector("#resultado-captcha").innerHTML= "Correcto";
+      console.log(nombre, telefono, email, consulta, captchaIngresado);
+    }else{
+      document.querySelector("#resultado-captcha").innerHTML= "No existen coincidencias";
+      generarCaptcha();
+    }
     
   }
